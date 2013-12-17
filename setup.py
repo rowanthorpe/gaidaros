d5f72ec30b4b22d7f60972d36415e04c41057499
Rowan Thorpe <rowan@rowanthorpe.com>
2013-12-17 18:41:18 +0200

    Bump version number of setup-with-teeth used

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from __future__ import unicode_literals, with_statement
 #
 # See README.rst for more information.
 #
-# This setup.py and file hierarchy is from the setup-with-teeth project (v0.2.6):
+# This setup.py and file hierarchy is from the setup-with-teeth project (v0.2.7):
 #  https://github.com/rowanthorpe/setup-with-teeth
 #  replace all XXXXXXX with your own text
 
